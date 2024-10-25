8956ac72283dc2cf7c6c17459990e959949fe81e
mehdizhd11 <98504734+mehdizhd11@users.noreply.github.com>
2024-10-25 13:48:12 +0330

    complete ETL

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.functions import col, max, year, row_number
 
 
 # Extractor from MsSQL
@@ -23,7 +24,46 @@ connection_properties = {
 query = f"(SELECT * FROM {table_name}) AS temp"
 
 df = spark.read.jdbc(url=jdbc_url, table=query, properties=connection_properties)
+df.cache()
 
 # Transform to pandas DataFrame
 pd_df = df.toPandas()
-print(pd_df)
+
+# Max High Low difference
+print("Max High Low difference")
+df.createOrReplaceTempView(f"{table_name}_view")
+max_high_low_df = spark.sql(f"""
+    SELECT DATE, HIGH - LOW AS HIGH_LOW 
+    FROM {table_name}_view 
+    WHERE HIGH - LOW = (SELECT MAX(HIGH - LOW) FROM {table_name}_view)
+""")
+max_high_low_df.show()
+
+# DataFrame API Query
+df_with_diff = df.withColumn("HIGH_LOW", col("HIGH") - col("LOW"))
+max_high_low_value = df_with_diff.agg(max("HIGH_LOW")).collect()[0][0]
+max_high_low_df = df_with_diff.filter(col("HIGH_LOW") == max_high_low_value)
+max_high_low_df.select("DATE", "HIGH_LOW").show()
+
+# Identify the top 3 trading days (by volume) each year,
+# along with the corresponding High, Low, and the percentage change between Open and Close prices for those days.
+print("Top trading days in year")
+top_days_df = spark.sql(f"""
+    WITH DateGroup AS (
+        SELECT DATE, HIGH, LOW, VOLUME, (CLOSE - OPEN) / CLOSE * 100 AS PC,
+               ROW_NUMBER() OVER (PARTITION BY YEAR(DATE) ORDER BY VOLUME DESC) AS RowNum
+        FROM {table_name}_view
+    )
+    SELECT * 
+    FROM DateGroup 
+    WHERE RowNum < 4 
+    ORDER BY YEAR(DATE), RowNum
+""")
+top_days_df.show()
+
+# DataFrame API Query
+window_spec = Window.partitionBy(year("DATE")).orderBy(col("VOLUME").desc())
+df_with_row_num = df.withColumn("ROW_NUM", row_number().over(window_spec))
+top_3_per_year = df_with_row_num.filter(col("ROW_NUM") <= 3)
+final_top = top_3_per_year.withColumn("PC", (col("CLOSE") - col("OPEN")) / col("CLOSE") * 100)
+final_top.select("DATE", "HIGH", "LOW", "VOLUME", "PC", "ROW_NUM").show()
